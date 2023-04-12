cee5c3aef67ca7342b9634c923f8fac8f0b85982
RaphaelMacedoB <lucasmacedoes@gmail.com>
2023-04-12 17:44:38 -0300

    feat: consume API VIACEP

--- js/scripts.js
+++ js/scripts.js
@@ -17,3 +17,34 @@ cepInput.addEventListener("keypress", (e) => {
         return;
     }
 })
+
+//Get address event
+cepInput.addEventListener("keyup", (e) => {
+    const inputValue = e.target.value
+        //Check if we have correct length
+    if (inputValue.length === 8) {
+        getAddress(inputValue)
+    }
+})
+
+//Get customer Address from API
+const getAddress = async(cep) => {
+    toggleLoader();
+    cepInput.blur();
+
+    const apiUrl = `https://viacep.com.br/ws/${cep}/json`
+    const response = await fetch(apiUrl)
+    const data = await response.json()
+
+
+};
+
+//Show or hide loader
+
+const toggleLoader = () => {
+    const fadeElement = document.querySelector("#fade")
+    const loaderElement = document.querySelector("#loader")
+
+    fadeElement.classList.toggle("hide")
+    loaderElement.classList.toggle("hide")
+}
